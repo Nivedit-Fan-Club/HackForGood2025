c125e188afcd54c8d0410b2b6d79cce6fa8176b2
limkongkiat <102126318+limkongkiat@users.noreply.github.com>
2025-01-16 01:58:56 +0800

    idk anymoe

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -19,8 +19,6 @@ import React, { Component } from "react";
 import { useLocation } from "react-router-dom";
 import { Navbar, Container, Nav, Dropdown, Button } from "react-bootstrap";
 
-import routes from "routes.js";
-
 function Header() {
   const location = useLocation();
   const mobileSidebarToggle = (e) => {
